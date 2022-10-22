603d779d996e91552269448d603294e47eb838cf
Kyer Potts <kyerpotts@gmail.com>
2022-10-22 14:25:14 +0800

    AuthDataLayer completed

--- Authenticator/AuthBusinessLayer.cs
+++ Authenticator/AuthBusinessLayer.cs
@@ -29,5 +29,61 @@ namespace Authenticator
         {
             throw new NotImplementedException();
         }
+        public int ValidateLogin(string username, string password)
+        {
+            int loginToken = 0;
+            // A dictionary is used to build the map of user credentials. This will allow the function to easily parse and match usernames and passwords
+            Dictionary<string, string> credentialsDictionary = new Dictionary<string, string>();
+
+            // A validation file must exist for validation to occur
+            if (File.Exists(credentialsFilePath))
+            {
+                using (StreamReader sr = File.OpenText(credentialsFilePath))
+                {
+                    string readString = String.Empty;
+                    while ((readString = sr.ReadLine()) != null)
+                    {
+                        string[] credTokens = readString.Split(':');
+                        credentialsDictionary.Add(credTokens[0], credTokens[1]);
+                    }
+                }
+
+                // If a match is found for login, the temporary validation token is saved to the file and the token is then returned to the caller
+                if (this.matchCredentials(username, password, credentialsDictionary))
+                {
+                    loginToken = validationTokenNum++;
+                    writeValToken(loginToken);
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+
+            // Returns either a valid token (greater than 0) or an invalid token (0) depending on the outcome of the validation
+            return loginToken;
+        }
+
+        // The validation token needs to be written to the token file each time login is authenticated.
+
+        // Supplied credentials must be matched against the registered credential values, if supplied credentials don't match, authentication must be invalid
+        private bool matchCredentials(string username, string password, Dictionary<string, string> credDictionary)
+        {
+            bool foundMatch = false;
+            string matchPassword;
+
+            // The credentials dict must be checked to ensure the user has registered by username, if not login credentials are invalid
+            if (credDictionary.TryGetValue(username, out matchPassword))
+            {
+                // If the username exists in the dictionary, the given password and the stored password must match in order to produce a valid result
+                if (password.Equals(matchPassword))
+                {
+                    foundMatch = true;
+                }
+            }
+
+            // If foundMatch returns true, the password supplied was valid and the user can be authenticated.
+            return foundMatch;
+        }
     }
 }
--- Authenticator/AuthDataLayer.cs
+++ Authenticator/AuthDataLayer.cs
@@ -9,39 +9,39 @@ namespace Authenticator
 {
     internal class AuthDataLayer
     {
-        private string validationFilePath;
-        private string tokenFilePath;
+        private string credentialsFilePath;
+        private string valTokenFilePath;
         private int validationTokenNum;
 
         // File paths and data files initialized and created here as they may need to be accessed immediately on server init.
         public AuthDataLayer()
         {
             // Saves the authentication files to "MyDocuments" for demo purposes
-            validationFilePath = Path.Combine(Environment.SpecialFolder.MyDocuments + @"\" + "authValidationFile.txt");
-            tokenFilePath = Path.Combine(Environment.SpecialFolder.MyDocuments + @"\" + "authTokenFile.txt");
+            credentialsFilePath = Path.Combine(Environment.SpecialFolder.MyDocuments + @"\" + "authCredentialsFile.txt");
+            valTokenFilePath = Path.Combine(Environment.SpecialFolder.MyDocuments + @"\" + "authTokenFile.txt");
 
             // validationToken will always begin at 0. 0 will be used as an invalid token, meaning that the credentials supplied were not authenticated
             validationTokenNum = 0;
 
             // Need to check if credentials file exists before creating to ensure user data is not wiped
-            if (!File.Exists(validationFilePath))
+            if (!File.Exists(valTokenFilePath))
             {
-                File.CreateText(validationFilePath);
+                File.CreateText(valTokenFilePath);
             }
 
             // Token file will always be invalid when server starts up so will always need to be overwritten if it exists.
-            File.CreateText(tokenFilePath);
+            File.CreateText(valTokenFilePath);
         }
 
         // The user credentials supplied are appended to an existing file, delineating each string with a ":" character in order to parse later
         public void registerNewCredentials(string username, string password)
         {
             // Credentials should only be saved to an existing file. If the file does not exist, there is some problem.
-            if (File.Exists(validationFilePath))
+            if (File.Exists(credentialsFilePath))
             {
-                using (StreamWriter sw = File.AppendText(validationFilePath))
+                using (StreamWriter sw = File.AppendText(credentialsFilePath))
                 {
-                    sw.WriteLine(username + ":" + password);
+                    sw.WriteLine(username + ":" + password + Environment.NewLine);
                 }
             }
             else
@@ -50,10 +50,67 @@ namespace Authenticator
             }
         }
 
-
-        public int ValidateCredentials(string username, string password)
+        public Dictionary<string, string> getCredDict()
         {
+            // A dictionary is used to build the map of user credentials. This will allow the function to easily parse and match usernames and passwords
+            Dictionary<string, string> credentialsDictionary = new Dictionary<string, string>();
 
+            // A validation file must exist for validation to occur
+            if (File.Exists(credentialsFilePath))
+            {
+                using (StreamReader sr = File.OpenText(credentialsFilePath))
+                {
+                    string readString = String.Empty;
+                    while ((readString = sr.ReadLine()) != null)
+                    {
+                        string[] credTokens = readString.Split(':');
+                        credentialsDictionary.Add(credTokens[0], credTokens[1]);
+                    }
+                }
+            }
+
+            return credentialsDictionary;
+        }
+
+        // The temp validation token needs to be written to the file whenever a valid login occurs
+        public void writeValToken(int valToken)
+        {
+            // If the tokenFile does not exist, an error has occured
+            if (File.Exists(valTokenFilePath))
+            {
+                using (StreamWriter sw = File.AppendText(valTokenFilePath))
+                {
+                    sw.WriteLine(valToken.ToString() + Environment.NewLine);
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+        // Parses the valToken file and returns a list of all currently valid tokens
+        public List<int> readValTokens()
+        {
+            List<int> valTokensList = new List<int>();
+            // 
+            if (File.Exists(valTokenFilePath))
+            {
+                using (StreamReader sr = File.OpenText(valTokenFilePath))
+                {
+                    string readString = String.Empty;
+                    while ((readString = sr.ReadLine()) != null)
+                    {
+
+                    }
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+
+            return valTokensList;
         }
     }
 }
